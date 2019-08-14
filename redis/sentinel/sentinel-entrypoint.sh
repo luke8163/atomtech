7468c5708ccbcfdd1974c8e0429d5b3f927ce9ed
Luke Liang <lukeliang@outlook.com>
2019-08-14 23:07:51 +0800

    complete redis-sentinel config

--- redis/sentinel/sentinel-entrypoint.sh
+++ redis/sentinel/sentinel-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sed -i "s/\$SENTINEL_QUORUM/$SENTINEL_QUORUM/g" /etc/redis/sentinel.conf
-sed -i "s/\$SENTINEL_DOWN_AFTER/$SENTINEL_DOWN_AFTER/g" /etc/redis/sentinel.conf
-sed -i "s/\$SENTINEL_FAILOVER/$SENTINEL_FAILOVER/g" /etc/redis/sentinel.conf
+sed -i "s/\$SENTINEL_QUORUM/$SENTINEL_QUORUM/g" /data/sentinel.conf
+sed -i "s/\$SENTINEL_DOWN_AFTER/$SENTINEL_DOWN_AFTER/g" /data/sentinel.conf
+sed -i "s/\$SENTINEL_FAILOVER/$SENTINEL_FAILOVER/g" /data/sentinel.conf
 
-exec docker-entrypoint.sh redis-server /etc/redis/sentinel.conf --sentinel
+exec /usr/local/bin/docker-entrypoint.sh redis-server /data/sentinel.conf --sentinel
